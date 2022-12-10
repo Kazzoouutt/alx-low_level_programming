feeada962a542b042ecdbac37eac78b5b927ca5a
Kazzoouutt <kazomd@live.co.uk>
2022-12-09 20:20:02 -0800

    Task 0 completed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* Positive anything is better than negative nothing */
 
 /**
- * main - Find out if a number stored in the variable is positive, negative or zero
+ * main - if a number stored in the variable is positive, negative or zero
  * Return: Always 0 (Success)
  */
 
@@ -15,12 +15,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n == 0)
 		printf("%i is zero\n", n);
 	else
-		printf("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	return (0);
 }
