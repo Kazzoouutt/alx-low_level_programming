86dec2c1d629f576d0d4c81cd3e62935beab96f1
Kazzoouutt <kazomd@live.co.uk>
2022-12-13 03:09:21 -0800

    Tasks 1 to 3 completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet - Writes a function that prints the alphabet in lowercase
+ * 		    followed by a newline
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char = x;
+	
+	for (x = "a"; x <= "z"; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - Prints ten times the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char a;
+
+	while (i++ < 10)
+	{
+		for (a = 'a', a <= 'z', a++)
+		{
+			_putchar(a);
+		}
+
+		_puchar('\n');
+	}
+}
