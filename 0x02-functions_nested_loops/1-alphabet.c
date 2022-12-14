0075261feed07966d23b0ba84022902ece043579
Kazzoouutt <kazomd@live.co.uk>
2022-12-13 20:25:13 -0800

    Betty coding style error cleared

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * print_alphabet - Writes a function that prints the alphabet in lowercase
- * 		    followed by a newline
+ * 			followed by a newline
  * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
 	char x;
-	
+
 	for (x = 'a'; x <= 'z'; x++)
 	{
 		_putchar(x);
