f387f169b9bc8df43b00362add3d1a20ec23aac3
Kazzoouutt <kazomd@live.co.uk>
2023-01-25 05:28:50 -0800

    Preprocessor updated

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * Author: Kazzouutt
+ * Description: Header file that defines a funtion-like macro
+ * SUM(x, y) that computes all the sums of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks exhibitung the use of preprocessor directives in C programming.
