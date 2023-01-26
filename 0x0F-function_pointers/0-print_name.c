78ff4d3334c72464930fa7e36422903046f7f002
Kazzoouutt <kazomd@live.co.uk>
2023-01-26 07:40:51 -0800

    Update 5

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name
- * @name: The string to add.
- * @f: a pointer to a function
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  *
- * Return: Nothig
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
-#include <stdlib.h>
-
 /**
- * Descr - Header file containing all the prototype function
+ * Author: Kazzoouutt
+ * About - Header file containing all the prototype function
  * used for completing the pointers to functions tasks.
  */
+
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
