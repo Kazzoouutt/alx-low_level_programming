36117c80a54c2bb5ef559ce9b2ff580c3863a5fe
Kazzoouutt <kazomd@live.co.uk>
2023-01-27 03:50:54 -0800

    Update 1 for Variadic functions

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains exercises showing the use of variadic functions va_list, va_arg, var_start and var_end.
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,32 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * Author: Kazzoouutt
+ * Descr: This header file contains the prototypes used
+ * in solving the exercises for Variadic functions.
+ */
+
+#include <stdarg.h>
+
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A stmbol representing a data type.
+ * @print: A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator,const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
